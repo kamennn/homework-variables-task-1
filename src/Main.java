031a624fcc158491f068b236d9e4776fe374a563
kamen <maks.kolesnikov.0202@mail.ru>
2022-09-20 14:36:18 +0300

    homework-variables-task-1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //задача 1//
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //задача 2//
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //задача 3//
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
